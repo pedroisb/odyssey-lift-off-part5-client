c18e99dfb7e687fe916c8944f234244182cdb6b7
Pedro Ivo Silva Brito <pedro.silva@globant.com>
2022-06-20 14:59:23 -0300

    sets ApolloClient.uri to heroku server

--- src/index.js
+++ src/index.js
@@ -5,8 +5,8 @@ import Pages from './pages';
 import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: 'http://localhost:4000/',
-  // uri: 'https://lift-off-part5-server.herokuapp.com/',
+  // uri: 'http://localhost:4000/',
+  uri: 'https://lift-off-part5-server.herokuapp.com/',
   cache: new InMemoryCache(),
 });
 
